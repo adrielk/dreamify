a7a7b8cadbaebee5f55638945f4e000adc213362
adrielk <theadrielkim@gmail.com>
2021-01-20 02:52:56 -0500

    typo

--- src/components/DreamCardComponent.js
+++ src/components/DreamCardComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap
+import { Card, Button } from 'react-bootstrap'
 
 //https://v4-alpha.getbootstrap.com/utilities/close-icon/ (code for x button)
 
